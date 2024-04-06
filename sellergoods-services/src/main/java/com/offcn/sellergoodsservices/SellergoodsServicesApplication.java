bba9941891402a6b997aa6e07bc689bb93421b38
浊羽 <bocxxxx@163.com>
2024-04-07 00:07:17 +0800

    创建其对应的子工程

---
+++ sellergoods-services/src/main/java/com/offcn/sellergoodsservices/SellergoodsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.offcn.sellergoodsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellergoodsServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellergoodsServicesApplication.class, args);
+    }
+
+}
---
+++ sellergoods-services/src/test/java/com/offcn/sellergoodsservices/SellergoodsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offcn.sellergoodsservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellergoodsServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
